7cfe5833016ed13a718a3b3761a957086f57f2a7
Tim Goodwin <tgoodwin@hioscar.com>
2021-04-02 14:21:20 -0400

    updates

--- install_fpp_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-[[ -d /usr/local/PathPicker ]] && exit 0
-
-cd /usr/local/
-git clone https://github.com/facebook/PathPicker.git
-ln -s /usr/local/PathPicker/fpp /usr/local/bin/fpp
